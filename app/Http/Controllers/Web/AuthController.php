a63e9a8f9dc1d9eab9b3ac1d86567fe6c203a7b0
wangning <416621723@qq.com>
2017-07-28 18:29:04 +0800

    2017年07月28日18:28:59

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -3,41 +3,87 @@
 namespace App\Http\Controllers\Web;
 
 use App\Model\User;
-use Request;
-use Illuminate\Http\Request as Re;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function signup()
+    public function signup(Request $request)
     {
-        $username  = Request::get('username');
-        $password  = Request::get('password');
-        $email = Request::get('email');
+        $user = new User();
+        $data = self::getRequest($request);
 
-        if (!(isset($username) && isset($password) && isset($email)))
+        if (!(isset($data['username']) && isset($data['password']) && isset($data['email'])))
             return ['status' => 0, 'msg' => '用户名、密码、邮箱都不能为空!'];
 
-        if ($this->where($username, 'name')->orWhere($email, 'email')->exists())
+        if ($user->where('name', $data['username'])->orWhere('email', $data['email'])->exists())
             return ['status' => 0, 'msg' => '用户名或邮箱已存在!'];
 
+        $user->name  = $data['username'];
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password']);
+
+        if ($user->save())
+            return [
+                'status'  => 1,
+                'msg'     => '注册成功!',
+                'user_id' => $user->id
+            ];
+        else
+            return [
+                'status' => 0,
+                'msg'    => '注册失败'
+            ];
+    }
+
+    public function signin(Request $request)
+    {
         $user = new User();
-        $user->name = $username;
-        $user->password = encrypt($password);
+        $data = self::getRequest($request);
 
+        if (empty($data['username']) || empty($data['password']))
+            return ['status' => 0, 'msg' => '请输入用户名或密码!'];
 
+        $userinfo = $user->where('name', $data['username'])
+                         ->orWhere('email', $data['email'])
+                         ->first();
 
+        if (empty($userinfo))
+            return ['status' => 0, 'msg' => '此用户不存在!'];
 
+        if (!Hash::check($data['password'], $userinfo['password']))
+            return ['status' => 0, 'msg' => '密码错误!'];
 
+        session(['userinfo' => [
+            'username' => $userinfo->name,
+            'user_id'   => $userinfo->id
+        ]]);
+
+        return ['status' => 1, 'msg' => '登录成功!'];
     }
 
-    public function signin()
+    public function signout(Request $request)
     {
+        $request->session()->flush();
 
+        if (!$request->session()->has('userinfo'))
+            return ['status' => 1, 'msg' => '登出成功!'];
+        return ['status' => 0, 'msg' => '登出失败!'];
     }
 
-    public function signout()
+    public static function check($request)
     {
+        return $request->session()->get('userinfo') ? true : false;
+    }
 
+    private static function getRequest($request)
+    {
+        return [
+            'username' => $request->input('username'),
+            'password' => $request->input('password'),
+            'email'    => $request->input('email')
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/api', function () {
-    return 'test api';
+//Route::get('/api', function () {
+//    return 'test api';
+//});
+
+Route::post('api/index', 'Web\AuthController@index'); //test
+
+Route::any('api/signup', 'Web\AuthController@signup');
+Route::any('api/signin', 'Web\AuthController@signin');
+Route::any('api/signout', 'Web\AuthController@signout');
+
+// Question
+Route::any('api/question/index', 'Web\QuestionController@index');
+Route::any('api/question/show', 'Web\QuestionController@show');
+
+Route::group(['middleware' => 'web_auth'], function () {
+    Route::any('api/question/create', 'Web\QuestionController@create');
+    Route::any('api/question/store', 'Web\QuestionController@store');
+    Route::any('api/question/edit', 'Web\QuestionController@edit');
+    Route::any('api/question/update', 'Web\QuestionController@update');
+    Route::any('api/question/destroy', 'Web\QuestionController@destroy');
 });
 
-Route::get('/api/signup', function () {
-    $db = new \App\Model\User();
-    $db->signup();
-});
+
